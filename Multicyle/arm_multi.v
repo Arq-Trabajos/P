7e48eb38c57af7f83ce86c807797acfbfe37a7bc
Mauricio <mauricio.croquet@utec.edu.pe>
2021-12-11 15:39:05 -0500

    reg_file

--- Multicyle/arm_multi.v
+++ Multicyle/arm_multi.v
@@ -576,3 +576,39 @@ module mux3 (
 	output wire [WIDTH - 1:0] y;
 	assign y = (s[1] ? d2 : (s[0] ? d1 : d0));
 endmodule
+
+module reg_file #(parameter W = 7, // Bit width - 1
+N = 15) // Number of registers - 1
+	(input clk, // clock
+	input reset, // reset
+	input reg_ena, // Write enable
+	input [W:0] data, // System input
+	input [3:0] rd, // Address for write
+	input [3:0] rs, // 1. read address
+	input [3:0] rt, // 2. read address
+	output reg [W:0] s, // 1. data
+	output reg [W:0] t); // 2. data
+
+	reg [W:0] r [0:N];
+	always @(posedge clk or posedge reset)
+	begin : mux3 // Input mux
+		integer k; // para loop
+		if (reset) // Asynchronous clear
+			for (k=0; k<=N; k=k+1) 
+				r[k] <= 0;
+		else if ((reg_ena == 1) && (rd > 0))
+			r[rd] <= data;
+	end
+	// 2 output demux sin registers
+	always @(*)
+	begin : DEMUX
+		if (rs > 0) // priemra fuente 
+			s = r[rs];
+		else
+			s = 0;
+		if (rt > 0) // segunda fuente
+			t = r[rt];
+		else
+			t = 0;
+	end
+endmodule
