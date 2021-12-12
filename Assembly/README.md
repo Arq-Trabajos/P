b6d85b601ec36c3c3b60e6aaaee0538355bb4cd1
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2021-12-12 09:49:03 -0500

    Organizacion de archivos multiplicadores

---
+++ Assembly/README.md
@@ -0,0 +1,7 @@
+# MULTIPLICADORES
+
+1. Introducción a multiplicadores
+2. Funciones
+   1. MUL
+   2. UMULL
+   3. SMULL
--- Assembly/introduccion_multiplicadores.s
+++ Assembly/introduccion_multiplicadores.s
@@ -12,3 +12,4 @@
 	; SMULL recibe valores de 32 bits y devuelve valores de 64 bits con valores de entrada con signo de completo dos
     MOV R1, #0x0111           ; R1 = -1 : 001 -> 110 -> 110 + 1 -> 111
     SMULL R2, R3, R1, R1      ; {R2,R3} = R1*R1  : R2 almacena el valor de un word y R3 almacena el valor de otro word, de esa manera se forma el 64 bits.
+
